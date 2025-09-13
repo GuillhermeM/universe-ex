d2d06a8f8841f728fbc7615fae7a8d368f982696
Guillherme Mota <guillherme.mota@ufpe.br>
2025-09-13 14:31:34 -0300

    refactor: remove totalPages prop and related pagination logic in PaginationControls component

--- src/components/PaginationControls.tsx
+++ src/components/PaginationControls.tsx
@@ -13,10 +13,9 @@ import {
 interface Props {
   page: number;
   hasNextPage: boolean;
-  totalPages: number;
 }
 
-export function PaginationControls({ page, hasNextPage, totalPages }: Props) {
+export function PaginationControls({ page, hasNextPage }: Props) {
   const searchParams = useSearchParams();
   const router = useRouter();
   const pathname = usePathname();
@@ -26,8 +25,6 @@ export function PaginationControls({ page, hasNextPage, totalPages }: Props) {
     params.set("page", newPage.toString());
     router.push(`${pathname}?${params.toString()}`);
   };
-
-  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
 
   return (
     <Pagination className="text-white">
@@ -45,20 +42,11 @@ export function PaginationControls({ page, hasNextPage, totalPages }: Props) {
           />
         </PaginationItem>
 
-        {pages.map((p) => (
-          <PaginationItem key={p}>
-            <PaginationLink
-              href="#"
-              isActive={p === page}
-              onClick={(e) => {
-                e.preventDefault();
-                if (p !== page) handlePageChange(p);
-              }}
-            >
-              {p}
-            </PaginationLink>
-          </PaginationItem>
-        ))}
+        <PaginationItem>
+          <PaginationLink href="#" isActive>
+            {page}
+          </PaginationLink>
+        </PaginationItem>
 
         <PaginationItem>
           <PaginationNext
